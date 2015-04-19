462063f8a61927a2699f8332ef4ec2e5b6aba106
Yarr <lohas1107@gmail.com>
2015-04-19 20:09:59 +0800

    save file

--- Evaluation/Program.cs
+++ Evaluation/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace Evaluation
     {
         static void Main(string[] args)
         {
+            if (args.Count() == 0) return;
+
+            File.AppendAllText("test.txt", "456" + Environment.NewLine);
+
+            Console.WriteLine("Finish!");
+            Console.ReadLine();
         }
     }
 }
